d625842906430ec7cd67635eea0cb66de1c8adb3
Prabin-rai <prabincham218@gmail.com>
2024-04-14 13:57:36 +0545

    check

--- app/components/AddMerchantButton.tsx
+++ app/components/AddMerchantButton.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 const AddMerchantButton: React.FC = () => {
   return (
     <Link href="/AddMerchantForm">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <button className="bg-green-700 hover:bg-green-500 text-white font-bold py-2 px-4 rounded">
         Add Merchant
       </button>
     </Link>
