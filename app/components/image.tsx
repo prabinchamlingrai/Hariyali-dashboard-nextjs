b729038b2eddd82d628b65f91860ecbf4351b3e7
Prabin-rai <prabincham218@gmail.com>
2024-04-14 13:12:15 +0545

    onbranch

--- app/components/image.tsx
+++ app/components/image.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const MyImageComponent: React.FC = () => {
   return (
-    <div className="w-40 mx-3.5 min-h-fit">
+    <div className="w-30 mx-3.5 min-h-fit">
       <Image
         src="/hariyali-abhiyan-logo-3.png"
         alt=""
